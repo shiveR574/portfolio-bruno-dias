deb13528191d1730bd5b9f5cecee7a0909725947
shiveR574 <bruno.dias1893@gmail.com>
2025-12-08 23:12:43 +0000

    Change2

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,9 @@
 import styles from "../css/About.module.css"
 import League from "../assets/league.png"
 import Valorant from "../assets/valorant.jpg"
+import React from "../assets/react.png"
+import HTML from "../assets/html.png"
+import CSS from "../assets/css.png"
 import CS2 from "../assets/cs2.png"
 
 function About() {
@@ -11,13 +14,25 @@ function About() {
                     About<span className={styles["highlight"]}>Me</span>
                 </h1>
                 <p>
-                    "[paragraph content]"
+                    Hey! I’m someone who loves gaming. I usually play 
+                    League of Legends, Valorant or CS2. 
+                    I really enjoy hanging out with friends, listening 
+                    to music and watch movies/series.
+                </p>
+                <p> I’m also really interested in IT and I see myself 
+                    building a career in programming, hopefully working 
+                    with React, HTML and CSS. I’m excited to grow in the 
+                    tech world and exploring design is another area 
+                    I’d love to dive into.
                 </p>
             </div>
             <div className={styles["right-content"]}>
                 <img src={League} alt="League of Legends" className={styles["Leagueoflegends"]}/>
                 <img src={Valorant} alt="Valorant" className={styles["Valorant"]}/>
                 <img src={CS2} alt="Counter-Strike 2" className={styles["CS2"]}/>
+                <img src={React} alt="React"  className={styles["React"]}/>
+                <img src={HTML} alt="HTML" className={styles["HTML"]}/>
+                <img src={CSS} alt="CSS" className={styles["CSS"]}/>
             </div>
         </div>
     )
